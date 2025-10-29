b26005c115d9ff7dd47db14ba95b955d55bb2e3c
luisandreiouano <110468848+LowisWano@users.noreply.github.com>
2025-10-29 21:23:05 +0800

    feat: add more efficient solution to products array except self

--- leetcode/products-array-except-self.py
+++ leetcode/products-array-except-self.py
@@ -7,3 +7,28 @@ class Solution:
             prod = reduce(lambda x, y: x * y, nums[0:i]+nums[i+1:len(nums)])
             output.append(prod)
         return output
+
+      
+# more efficient solution
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = []
+        accumprod = 1
+        for i in range(len(nums)):
+            prefix.append(accumprod)
+            accumprod *= nums[i]
+            
+
+        suffix = []
+        accumprod = 1
+        for i in range(len(nums)-1, -1, -1):
+            suffix.insert(0, accumprod)
+            accumprod *= nums[i]
+        
+        output = []
+        for i in range(len(nums)):
+            output.append(prefix[i] * suffix[i])
+
+        return output
+
